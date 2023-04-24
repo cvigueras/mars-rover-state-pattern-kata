2dfc00a4fc53efbc6fe86ae61d68c8c738e2cfc5
cvigueras <carlosj.vigueras@gmail.com>
2023-04-24 15:02:54 +0200

    Feat: Created super class state.

---
+++ MarsRover.App/IState.cs
@@ -0,0 +1,12 @@
+﻿namespace MarsRover.App
+{
+    abstract class IState
+    {
+        protected RemotelyControl? RemotelyControl;
+
+        public void SetContext(RemotelyControl? remotelyControl)
+        {
+            RemotelyControl = remotelyControl;
+        }
+    }
+}
